705ce0dfe65f82668289ee2b26efb7de2a7fa9b8
Daniel Lopez <canino9242@hotmail.com>
2022-10-11 19:32:09 -0500

    fix: add more case at regHour

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,6 +1,6 @@
 const regName = "[a-zA-Z]+";
 const regDay = "(MO|TU|WE|TH|FR|SA|SU)";
-const regHour = "(0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9])";
+const regHour = "(0?[0-9]|1[0-9]|2[0-3]|01[0-9]|02[0-9]):(0?[0-9]|[1-5][0-9])";
 const regSchedule = `${regDay}(${regHour}-${regHour})`;
 const regEmployee = `${regName}=(${regSchedule},)*(${regSchedule})$`;
 
